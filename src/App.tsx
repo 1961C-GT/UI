f47925c888a923e59c5508cdb2014623565b9d47
Kevin Abraham <abraha2d@users.noreply.github.com>
2019-02-09 00:27:05 -0500

    Add Prettier code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import React, {Component} from 'react';
-import {Icon, Layout, Menu} from "antd";
-import './App.css';
+import React, { Component } from "react";
+import { Icon, Layout, Menu } from "antd";
+import "./App.css";
 
-const {Header, Content} = Layout;
+const { Header, Content } = Layout;
 
 class App extends Component {
-    render() {
-        return (
-            <Layout className="layout">
-                <Header>
-                    <div className="logo"/>
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['map']}
-                        style={{lineHeight: '64px'}}
-                    >
-                        <Menu.Item key="map">Map</Menu.Item>
-                        <Menu.Item key="details">Details</Menu.Item>
-                        <Menu.Item key="settings"><Icon type="setting"/>Settings</Menu.Item>
-                    </Menu>
-                </Header>
-                <Content>
-                    <div>Map Goes Here</div>
-                </Content>
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["map"]}
+            style={{ lineHeight: "64px" }}
+          >
+            <Menu.Item key="map">Map</Menu.Item>
+            <Menu.Item key="details">Details</Menu.Item>
+            <Menu.Item key="settings">
+              <Icon type="setting" />
+              Settings
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content>
+          <div>Map Goes Here</div>
+        </Content>
+      </Layout>
+    );
+  }
 }
 
 export default App;
